72e9a351bb1d25c583e37156fc9215ee5d8c4140
Yash Raj <yashrj130@gmail.com>
2019-09-06 13:21:34 +0530

    customers-list

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from '../../shared/interfaces';
+import { SorterService } from 'src/app/core/sorter.service';
 
 @Component({
   selector: 'app-customers-list',
@@ -26,7 +27,7 @@ export class CustomersListComponent implements OnInit {
   customersOrderTotal: number;
   currencyCode: string = "USD";
 
-  constructor() { }
+  constructor(private sorterService: SorterService) { }
 
   ngOnInit() {
   }
@@ -54,6 +55,7 @@ export class CustomersListComponent implements OnInit {
 
   sort(prop: string) {
     // A sorter service will handle the sorting
+    this.sorterService.sort(this.filteredCustomers, prop);
   }
 
 }
